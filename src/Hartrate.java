54eba78966d029f5b8de1f4c42574ad41fd78002
MilkyasEyob.Habte <milkyas23>
2019-12-13 11:11:01 +0100

    nu är programet klar äntlägen

--- src/Hartrate.java
+++ src/Hartrate.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Hartrate {
+    //private static Object maxHeartRate;
+
     public static void main (String[] args) {
         Scanner input = new Scanner( System.in);
 
@@ -8,35 +10,14 @@ public class Hartrate {
         String namn = input.nextLine();
 
         System.out.println( "skriv din ålder");
-        String Ålser = input.nextLine();
+        int  Ålder = Integer.parseInt(input.nextLine());
 
-        Hartrate person1 = new Hartrate();
+        int maxHeartRate = 220 - Ålder;
 
-        System.out.printf("Calculations for %s %s age %d\n",
-                person1.Namn(), person1.ålder() );
-        System.out.printf( "Max Heart rate: %d BPM (beats per minute)\n", person1.getMaxHeartRate() ); // Here, you haven't set the maxHeartRate yet, so it will be 0.
+        System.out.printf("Calculations for %s  age %d\n",
+                namn, Ålder );
+        System.out.printf( "Max Heart rate: %d BPM (beats per minute)\n", maxHeartRate );
         input.close();
     }
-    private int ålder, maxHeartRate;
-    private String Namn;
-
-    public void displayTargetHeartRateRange() {
-
-    }
-
-    public Object getMaxHeartRate() {
-        return maxHeartRate;
-    }
-
-    public Object ålder() {
-        return ålder;
-    }
-
-    public Object Namn() {
-        return Namn;
-    }
-
-
-
-
 }
+
